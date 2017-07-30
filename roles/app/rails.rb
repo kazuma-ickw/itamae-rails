4296ea95c746f6fc5eb102af9d3f2a3ae8befd47
Kazuma Ichikawa <b.g.ibx.leo10m@gmail.com>
2017-07-30 19:00:56 +0900

    アプリケーションのリポジトリをcloneする処理を追加、MySQL関連をitamae-secretsで管理して.envを書く

--- roles/app/rails.rb
+++ roles/app/rails.rb
@@ -1,4 +1,23 @@
+require "itamae/secrets"
+
+node[:secrets] = Itamae::Secrets(File.join(__dir__, "../../secret"))
+
 package "mysql-devel" do
   user "root"
   action :install
+end
+
+git node["app_dir"] do
+  repository node["repository"]
+  destination node["app_dir"]
+end
+
+template "create .env" do
+  action :create
+  source "templates/.env.erb"
+  path node["app_dir"] + "/.env"
+  variables(
+    mysql_user_name: node[:secrets][:mysql_user_name],
+    mysql_password: node[:secrets][:mysql_password]
+  )
 end
